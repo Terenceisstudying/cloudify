bf939736acf8df6ff7d06364f05e9dbaeca924a1
ahhlxlx <lixuankwek@gmail.com>
2026-03-11 18:10:15 +0800

    change port number

--- services/emailService.js
+++ services/emailService.js
@@ -7,7 +7,8 @@ class EmailService {
     constructor() {
         this.transporter = nodemailer.createTransport({
             host: process.env.EMAIL_HOST,
-            port: parseInt(process.env.EMAIL_PORT || '587'),
+            // port: parseInt(process.env.EMAIL_PORT || '587'),
+            port: 465,
             secure: false, // true for 465, false for other ports
             auth: {
                 user: process.env.EMAIL_USER,
