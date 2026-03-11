f15b00adbd89d17d508c8bc11aa50aa0fbec24b8
ahhlxlx <lixuankwek@gmail.com>
2026-03-11 18:33:19 +0800

    resend

--- services/emailService.js
+++ services/emailService.js
@@ -5,15 +5,26 @@ dotenv.config();
 
 class EmailService {
     constructor() {
+        // this.transporter = nodemailer.createTransport({
+        //     host: process.env.EMAIL_HOST,
+        //     port: parseInt(process.env.EMAIL_PORT || '587'),
+        //     secure: false, // true for 465, false for other ports
+        //     auth: {
+        //         user: process.env.EMAIL_USER,
+        //         pass: process.env.EMAIL_PASSWORD,
+        //     },
+        //     logger: true,
+        //     debug: true
+        // });
         this.transporter = nodemailer.createTransport({
             host: process.env.EMAIL_HOST,
-            // port: parseInt(process.env.EMAIL_PORT || '587'),
-            port: 465,
-            secure: false, // true for 465, false for other ports
+            port: parseInt(process.env.EMAIL_PORT || '587'),
+            secure: false,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASSWORD,
             },
+            requireTLS: true,
             logger: true,
             debug: true
         });
@@ -246,7 +257,7 @@ class EmailService {
         </html>`;
 
         const mailOptions = {
-            from: `"Singapore Cancer Society" <${process.env.EMAIL_USER}>`,
+            from: process.env.EMAIL_FROM,
             to,
             subject: `Your ${isGeneric ? 'General' : assessmentType?.charAt(0).toUpperCase() + assessmentType?.slice(1) || ''} Cancer Risk Assessment Results`,
             html: htmlContent
